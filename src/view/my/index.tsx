0663330ef60462ac1e76d834d3278382cde8610c
hehejie <2823886877@qq.com>
2020-09-22 09:10:35 +0800

    feat: my view

--- src/view/my/index.tsx
+++ src/view/my/index.tsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
-
+import { SettingOutlined, HighlightOutlined, CloseCircleOutlined, QuestionCircleOutlined, ImportOutlined, RightOutlined } from '@ant-design/icons';
 import "./index.scss"
-
 
 interface IProps{
 
@@ -10,8 +9,6 @@ interface IProps{
 interface IState{
     msg: string
 }
-
-
 
 export default class Index extends Component<IProps, IState>{
     constructor(props: any){
@@ -24,15 +21,21 @@ export default class Index extends Component<IProps, IState>{
         return (
             <div className="hh-my">
                 <section>
-                    <div>3D用户头像</div>
-                    <div><span>呵呵杰</span><span>水瓶座</span></div>
+                    <CloseCircleOutlined className="hh-close" />
                 </section>
-                <section>
-                    <ul>
-                        <li><a href="/my/setting" className="hh-btn">设置</a></li>
-                        <li><a href="/my/goal" className="hh-btn">我的目标</a></li>
-                        <li><a href="/my/help" className="hh-btn">帮助</a></li>
-                        <li><a href="/login" className="hh-btn">退出</a></li>
+                <section className="hh-avatar">
+                    <div className="hh-avatar-gl">3D用户头像</div>
+                    <div className="hh-avatar-info">
+                        <span>呵呵杰</span>
+                        <span>水瓶座</span>
+                    </div>
+                </section>
+                <section className="hh-options">
+                    <ul className="hh-groups">
+                        <li className="hh-row"><a href="/my/setting" className="hh-btn-item"><SettingOutlined className="icon" />设置<RightOutlined className="fr" /></a></li>
+                        <li className="hh-row"><a href="/my/goal" className="hh-btn-item"><HighlightOutlined className="icon" />我的目标<RightOutlined className="fr" /></a></li>
+                        <li className="hh-row"><a href="/my/help" className="hh-btn-item"><QuestionCircleOutlined className="icon" />帮助<RightOutlined className="fr" /></a></li>
+                        <li className="hh-row"><a href="/login" className="hh-btn-item"><ImportOutlined className="icon" />退出<RightOutlined className="fr" /></a></li>
                     </ul>
                 </section>
             </div>
--- src/view/my/setting/index.tsx
+++ src/view/my/setting/index.tsx
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import { Switch } from 'antd';
+import { CloseOutlined, CheckOutlined } from '@ant-design/icons';
+
 
 import "./index.scss"
 
@@ -24,11 +27,31 @@ export default class Index extends Component<IProps, IState>{
         return (
             <div className="hh-goal">
                 <section>
-                    <ul>
-                        <li>是否被别人检索</li>
-                        <li>是否被别人查看okr</li>
-                        <li>全局主题</li>
-                        <li>版本信息</li>
+                    <ul className="hh-groups">
+                        <li className="hh-row">
+                            span是否被别人检索
+                            <Switch
+                                className="fr"
+                                checkedChildren={<CheckOutlined />}
+                                unCheckedChildren={<CloseOutlined />}
+                                defaultChecked
+                            />    
+                        </li>
+                        <li className="hh-row">是否被别人查看okr <Switch
+                                className="fr"
+                                checkedChildren={<CheckOutlined />}
+                                unCheckedChildren={<CloseOutlined />}
+                                defaultChecked
+                            />    </li>
+                        <li className="hh-row">全局主题
+                            <input type="color" className="fr"/>
+                        </li>
+                        <li className="hh-row">版本信息 <Switch
+                                className="fr"
+                                checkedChildren={<CheckOutlined />}
+                                unCheckedChildren={<CloseOutlined />}
+                                defaultChecked
+                            />    </li>
                     </ul>
                 </section>
             </div>
