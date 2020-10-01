696450272e67757eaba2c1b5a1759e3fc58a6c4c
hehejie <2823886877@qq.com>
2020-10-01 12:29:55 +0800

    修复了tabbug

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Router from './router/router'
 import './App.scss';
+import './assert/style/csshake.css'
+
+
+const Sharke = React.createContext("sharke")
 
 function App() {
   return (
     <div className="hh-box">
-      <Router />
+      <Sharke.Provider value="1">
+        <div className="hh-container">
+          <Router />
+        </div>
+      </Sharke.Provider>
+      
     </div>
   );
 }
-
 export default App;
